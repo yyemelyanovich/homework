0632312bc3df40e82162194f1168ea52f07a78ae
Yuliya Yemelyanovich <yyemelyanovich@infoblox.com>
2021-06-22 16:55:38 +0300

    [task2] Completed task 2
    Implemented functions to comply with the tests using sort package.

--- module-3/task2/slices/slice.go
+++ module-3/task2/slices/slice.go
@@ -1,10 +1,18 @@
 package slices
 
+import (
+	"sort"
+)
+
 func SortStringsInPlace(s []string) {
+	sort.Strings(s)
 }
 
 func SortStringsPure(s []string) []string {
-
+	s1 := make([]string, len(s))
+	copy(s1, s)
+	sort.Strings(s1)
+	return s1
 }
 
 type User struct {
@@ -13,9 +21,19 @@ type User struct {
 }
 
 func SortUsersPure(s []User) []User {
+	r := make([]User, len(s))
+	copy(r, s)
+	sort.Slice(r, func(i, j int) bool {
+		return (r[i].FirstName + r[i].LastName) < (r[j].FirstName + r[j].LastName)
+	})
 	return r
 }
 
 func SortUsersPureDesc(s []User) []User {
+	r := make([]User, len(s))
+	copy(r, s)
+	sort.Slice(r, func(i, j int) bool {
+		return (r[i].FirstName + r[i].LastName) > (r[j].FirstName + r[j].LastName)
+	})
 	return r
 }
