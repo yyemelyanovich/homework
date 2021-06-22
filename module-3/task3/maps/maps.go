d2b7bdf4d5a2c6114500eb898c82c770be497b40
Yuliya Yemelyanovich <yyemelyanovich@infoblox.com>
2021-06-22 18:56:07 +0300

    [task3] Completed task 3
    Implemented function to pass the test using sort package.

--- module-3/task3/maps/maps.go
+++ module-3/task3/maps/maps.go
@@ -1,4 +1,18 @@
 package maps
 
+import (
+	"sort"
+)
+
 func SortedValues(m map[int]string) string {
+	var out string
+	s := make([]int, 0, len(m))
+	for i, _ := range m {
+		s = append(s, i)
+	}
+	sort.Ints(s)
+	for _, i := range s {
+		out = out + m[i]
+	}
+	return out
 }
