58a4d9abec9d24cead2e784afcabf270b3e41f2b
Yuliya Yemelyanovich <yyemelyanovich@infoblox.com>
2021-06-22 16:00:24 +0300

    [task1] Completed task 1
    Canged function's argument to a pointer to array and fixed tests to pass
    a pointer and compare values.

--- module-3/task1/pointers/pointers_test.go
+++ module-3/task1/pointers/pointers_test.go
@@ -14,9 +14,8 @@ func Test_doubleValues(t *testing.T) {
 	}
 
 	for i, tt := range tests {
-		out, expected := *tt.in, *tt.expected
-		if DoubleValues(tt.in); expected == out {
-			t.Errorf("Unexpected result in Test #%d, expected %+v, got %+v", i, tt.expected, *tt.in)
+		if DoubleValues(&tt.in); tt.expected != tt.in {
+			t.Errorf("Unexpected result in Test #%d, expected %+v, got %+v", i, tt.expected, tt.in)
 		}
 	}
 }
